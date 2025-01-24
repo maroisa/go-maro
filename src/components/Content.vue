93f5252c353462308079cf0efda4539f02c4adda
maroisa <ahmadroisul412@gmail.com>
2025-01-25 06:11:28 +0700

    URL validation

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,11 +1,35 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+const urlValid = ref(false)
+const urlValue = ref("")
+
+watch(urlValue, (newValue) => {
+    const pattern = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
+    urlValid.value = pattern.test(newValue)
+})
+
+</script>
+
 <template>
     <main class="col container">
         <h1>Link Shortener</h1>
-        <input type="text" placeholder="https://siakad.uns.ac.id">
+        <input 
+        :class="!urlValid ? 'red' : ''"
+        v-model="urlValue" 
+        type="text" 
+        placeholder="https://siakad.uns.ac.id">
     </main>
 </template>
 
 <style scoped>
+
+.container {
+    flex-grow: 1;
+    align-items: center;
+    justify-content: center;
+    gap: 4rem;
+}
 
 input:focus {
     outline: 3px solid rgba(0, 100, 0, 1);
@@ -22,11 +46,8 @@ input {
     transition: all 100ms;
 }
 
-.container {
-    flex-grow: 1;
-    align-items: center;
-    justify-content: center;
-    gap: 4rem;
+.red:focus {
+    outline-color: red;
 }
 
 </style>
